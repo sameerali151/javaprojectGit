125c2e14249174bfb644592b8ab142f019e488b5
sameer <syedjava28@gmail.com>
2019-10-27 19:59:36 -0400

    first Git commit with java project

---
+++ src/Hellosample.java
@@ -0,0 +1,10 @@
+public class Hellosample {
+    public static void main(String[] args){
+        System.out.println("Hello world  + This is my first git project with intellij");
+
+
+
+
+
+    }
+}
